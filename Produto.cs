149881640fc333da8f6a8f9a9bb502c895d3879a
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-06-29 16:45:30 -0300

    Criando arquivo csv Excel finalizado

---
+++ Produto.cs
@@ -0,0 +1,35 @@
+using System.IO;
+
+namespace Aula27_28_29_30
+{
+    public class Produto
+    {
+        
+        public int Codigo {get;set;}
+        public string Nome {get;set;}
+        public float Preco {get;set;}
+
+        private const string PATH = "Database/Produto.csv";
+
+        //criando pasta Database manualmente
+        public Produto()
+        {
+
+            if(!File.Exists(PATH))
+            {
+                File.Create(PATH).Close();
+            }
+        
+        }
+
+        public void Inserir(Produto p){
+            var linha = new string [] {p.PrepararLinhaCSV(p)};
+            File.AppendAllLines(PATH, linha);
+        }
+
+        private string PrepararLinhaCSV(Produto produto){
+
+            return $"Cod= {produto.Codigo}; Nome = {produto.Nome}; Preco = {produto.Preco}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            p.Codigo = 1;
+            p.Nome = "AudioBook Revolta de Atlas";
+            p.Preco = 21.98f;
+
+            p.Inserir(p);
+
+        }
+    }
+}
