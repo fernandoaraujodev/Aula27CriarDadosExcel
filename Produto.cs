6188ea2962514c2aec2e79853d5df7c067ca05e3
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-06-30 22:24:42 -0300

    Aula sobre ler dados no Excel/csv finalizada

--- Produto.cs
+++ Produto.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System;
 
 namespace Aula27_28_29_30
 {
@@ -30,14 +32,72 @@ namespace Aula27_28_29_30
         
         }
 
+        /// <summary>
+        /// Adicionando novos produtos
+        /// </summary>
+        /// <param name="p"></param>
         public void Inserir(Produto p){
             var linha = new string [] {p.PrepararLinhaCSV(p)};
             File.AppendAllLines(PATH, linha);
         }
 
+        /// <summary>
+        /// Organizar os dados para ficarem em linhas (codigo=x; nome=a; preco=0)
+        /// </summary>
+        /// <param name="produto"></param>
+        /// <returns></returns>
         private string PrepararLinhaCSV(Produto produto){
 
             return $"Cod= {produto.Codigo}; Nome = {produto.Nome}; Preco = {produto.Preco}";
         }
+
+        //-------------------------------------------------------
+        //Aula28 - Ler Dados Excel
+
+        public List<Produto> Ler(){
+
+            //Criamos uma lista de produtos
+            List<Produto> produtos = new List<Produto>();
+
+            //Transformamos as linhas encontradas em uma array de strings
+            string [] lista = File.ReadAllLines(PATH);
+
+            //Varremos este array de strings
+            foreach(var linha in lista){
+
+                //Quebramos cada linha da lista em partes
+                string[] dados = linha.Split(';'); //dividindo a string em cada ;
+
+                //Tratamento dos dados e adicionar um novo produto
+                Produto prod = new Produto();
+                prod.Codigo = Int32.Parse (SepararDados(dados[0]));
+                prod.Nome   = SepararDados(dados[1]);
+                prod.Preco  = float.Parse (SepararDados(dados[2]));
+
+                //Adicionar o produto tratado na lista                
+                produtos.Add(prod);
+
+            }
+
+            return produtos;
+
+        }
+
+
+        /// <summary>
+        /// Tratamento da array lista para obter somente nome, preço e código
+        /// </summary>
+        /// <param name="_coluna"></param>
+        /// <returns></returns>
+        private string SepararDados(string _coluna){
+            
+            //cada informação esta assim
+            //0       1
+            //Preco = The Last Of Us II
+            return _coluna.Split("=")[1];
+
+        }
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Aula27_28_29_30
 {
@@ -7,11 +8,20 @@ namespace Aula27_28_29_30
         static void Main(string[] args)
         {
             Produto p = new Produto();
-            p.Codigo = 1;
-            p.Nome = "AudioBook Revolta de Atlas";
-            p.Preco = 21.98f;
+            p.Codigo = 4;
+            p.Nome = "11 Cidades - Axel Torres";
+            p.Preco = 33.98f;
 
             p.Inserir(p);
+
+            List<Produto> lista1 = new List<Produto>();
+            lista1 = p.Ler();
+
+            foreach(Produto item in lista1){
+                Console.WriteLine($"Produto: {item.Nome} custa R$ {item.Preco}");
+            }
+
+
 
         }
     }
